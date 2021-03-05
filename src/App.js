701ab213076ad24c91436670218ee940bb3e9f0e
Lalita Sharma <lalitamsharma04@gmail.com>
2021-03-05 22:44:05 +0530

    lets see

--- src/App.js
+++ src/App.js
@@ -2,26 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name='Lalita';
   return (
-    <div>
-    <h1>lets try</h1>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello-world {name} how are u
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Cart</h1>
     </div>
   );
 }
