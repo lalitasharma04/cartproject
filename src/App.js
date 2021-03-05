d3de7aa57fbae3abf0ffaf75cda28c638a8c5505
Lalita Sharma <lalitamsharma04@gmail.com>
2021-03-06 00:36:07 +0530

    Added cartitem component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import CartItem from './CartItem';
 
 function App() {
   return (
     <div className="App">
       <h1>Cart</h1>
+      <CartItem />
     </div>
   );
 }
